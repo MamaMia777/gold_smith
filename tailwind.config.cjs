d14075784f12c1b37e344a9026486a0bb5744e26
Rol <rolikpolo34@gmail.com>
2023-04-27 11:38:05 +0300

    cg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "black",
+        primary: "#04504466",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
